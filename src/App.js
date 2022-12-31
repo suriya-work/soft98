0067688a9e941d4e14fb88c79ff574c8f867cbec
suriya babaei <suriyalifework60@gmail.com>
2022-12-31 03:16:23 -0800

    create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+//Components
+import Header from './components/Header';
 const App = () => {
   return (
     <div>
-
+      <Header />
     </div>
   );
 };
