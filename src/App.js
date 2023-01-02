c929ca3e3b1e78e87e70f07bfb2c74ff88e8464e
suriya babaei <suriyalifework60@gmail.com>
2023-01-02 06:54:30 -0800

    header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 //Components
 import Header from './components/Header';
+import Banner from './components/Banner';
 import Footer from './components/Footer';
 const App = () => {
   return (
     <div>
       <Header />
+      <Banner />
       <Footer />
     </div>
   );
